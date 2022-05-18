36b4590dc8d5db9fb8e6b0fb240401618566cd8f
dclark <dclark@student.42.fr>
2022-05-18 16:53:10 +0200

    lavage de cerveau des containers

--- includes/ft_vector.h
+++ includes/ft_vector.h
@@ -6,34 +6,19 @@
 /*   By: dclark <dclark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 11:49:13 by dclark            #+#    #+#             */
-/*   Updated: 2022/05/18 13:43:57 by dclark           ###   ########.fr       */
+/*   Updated: 2022/05/18 16:33:40 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <>
 
 namespace ft {
-	template<class T, class std::allocator<T>>
+	template<class T, class Alloc std::allocator<T>>
 	class vector {
 		public:
 			// Constructor (Surcharge de constructor ?)
 			// Destructor (Surcharge de destructor ?)
 			// Operator =
-
-			/*-------- Member_type --------*/
-			T										value_type				
-			std::allocator::value_type				allocator_type			
-			std::allocator::reference				reference
-			std::allocator::const_reference			const_reference
-			std::allocator::pointer					pointer
-			std::allocator::const_pointer			const_pointer
-			//iterator
-			//cosnt_iterator
-			signed int								difference_type
-			std::reverse_iterator<iterator>			reverse_iterator
-			std::reverse_iterator<const_iterator>	const_reverse_iterator
-			signed int							difference_type
-			unsigned int							size_type
 
 			/*-------- Iterators --------*/
 			// begin
@@ -73,6 +58,21 @@ namespace ft {
 			// Operator <=
 			// Operator >
 			// Operator >=
-			// swap
+			// swap			
+
+			/*-------- Member_type --------*/
+			T										value_type				
+			std::allocator::value_type				allocator_type			
+			std::allocator::reference				reference
+			std::allocator::const_reference			const_reference
+			std::allocator::pointer					pointer
+			std::allocator::const_pointer			const_pointer
+			//iterator
+			//cosnt_iterator
+			signed int								difference_type
+			std::reverse_iterator<iterator>			reverse_iterator
+			std::reverse_iterator<const_iterator>	const_reverse_iterator
+			signed int							difference_type
+			unsigned int							size_type
 	};
 }
