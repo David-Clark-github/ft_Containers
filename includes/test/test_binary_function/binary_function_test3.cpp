44ff0d7aab27fdd9cf0de1051e85a10b6d84fc0a
Dave <david.clark@dbmail.com>
2022-06-04 22:37:01 +0200

    correction des fonctions a tester de std:: a ft::

--- includes/test/test_binary_function/binary_function_test3.cpp
+++ includes/test/test_binary_function/binary_function_test3.cpp
@@ -15,7 +15,7 @@ int main() {
 		std::pair<int*,int*> ptiter;
 		int foo[]={10,20,30,40,50};
 		int bar[]={10,20,40,80,160};
-		ptiter = std::mismatch (foo, foo+5, bar, std::equal_to<int>());
+		ptiter = std::mismatch (foo, foo+5, bar, ft::equal_to<int>());
 		std::cout << "First mismatching pair is: " << *ptiter.first;
 		std::cout << " and " << *ptiter.second << '\n';
 	}
@@ -24,7 +24,7 @@ int main() {
 	std::cout << RED "\nNOT_EQUAL_TO\n" NC;
 	{
 		int numbers[]={10,10,10,20,20};
-		int* pt = std::adjacent_find (numbers, numbers+5, std::not_equal_to<int>()) +1;
+		int* pt = std::adjacent_find (numbers, numbers+5, ft::not_equal_to<int>()) +1;
 		std::cout << "The first different element is " << *pt << '\n';
 	}
 
@@ -43,7 +43,7 @@ int main() {
 	std::cout << RED "\nLESS_EQUAL\n" NC;
 	{
 		int numbers[]={25,50,75,100,125};
-		int cx = std::count_if (numbers, numbers+5, std::bind2nd(std::less_equal<int>(),100));
+		int cx = std::count_if (numbers, numbers+5, std::bind2nd(ft::less_equal<int>(),100));
 		std::cout << "There are " << cx << " elements lower than or equal to 100.\n";
 	}
 
@@ -51,7 +51,7 @@ int main() {
 	std::cout << RED "\nGREATER\n" NC;
 	{
 		int numbers[]={20,40,50,10,30};
-		std::sort (numbers, numbers+5, std::greater<int>());
+		std::sort (numbers, numbers+5, ft::greater<int>());
 		for (int i=0; i<5; i++)
 			std::cout << numbers[i] << ' ';
 		std::cout << '\n';
@@ -61,7 +61,7 @@ int main() {
 	std::cout << RED "\nGREATER_EQUAL\n" NC;
 	{
 		int numbers[]={20,-30,10,-40,0};
-		int cx = std::count_if (numbers, numbers+5, std::bind2nd(std::greater_equal<int>(),0));
+		int cx = std::count_if (numbers, numbers+5, std::bind2nd(ft::greater_equal<int>(),0));
 		std::cout << "There are " << cx << " non-negative elements.\n";
 	}
 
