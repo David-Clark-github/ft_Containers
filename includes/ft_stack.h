7e223a667a08987fb1d8da33de26c93b9a20a733
Dave <david.clark@dbmail.com>
2022-06-18 23:18:48 +0200

    stack fait, et en cours de test, STD dans ft::stack ne marche pas :/ mais ft::vector oui ...

--- includes/ft_stack.h
+++ includes/ft_stack.h
@@ -6,7 +6,7 @@
 /*   By: david <dclark@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 10:04:35 by david             #+#    #+#             */
-/*   Updated: 2022/06/18 10:11:14 by david            ###   ########.fr       */
+/*   Updated: 2022/06/18 14:55:09 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,78 @@
 namespace ft {
 	template <class T, class Container = ft::vector<T> >
 	class stack {
+		public:
 		/*-------- Member types --------*/
-		typedef T			value_type;
-		typedef Container	container_type;
-		typedef size_t		size_type;
+			typedef Container							container_type;
+			typedef typename Container::value_type		value_type;
+			typedef typename Container::size_type		size_type;
+			typedef typename Container::reference		reference;
+			typedef typename Container::const_reference	const_reference;
 
-		/*-------- Constructor --------*/
-		explicit stack (const container_type& ctnr = container_type()) {
+			/*-------- Constructor / Destructor --------*/
+			explicit stack (const container_type& ctnr = container_type()) : c(ctnr) {}
 			
-		}
-	}
-}
+			~stack(void) {}
+
+			/*-------- Member functions --------*/
+			bool empty() const {
+				return (c.empty());
+			}
+
+			size_type size() const {
+				return (c.size());
+			}
+
+			value_type& top() {
+				return (c.back());
+			}
+
+			const value_type& top() const {
+				return (c.back);
+			}
+
+			void push (const value_type& val) {
+				c.push_back(val);
+			}
+
+			void pop() {
+				c.pop_back();
+			}
+
+			/*-------- Non-member function overloads --------*/
+			// ==
+			friend bool operator== (const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+				return lhs.c == rhs.c;
+			}
+
+			// !=
+			friend bool operator!= (const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+				return lhs.c != rhs.c;
+			}
+
+			// <
+			friend bool operator<  (const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+				return lhs.c < rhs.c;
+			}
+
+			// <=
+			friend bool operator<= (const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+				return lhs.c <= rhs.c;
+			}
+
+			// >
+			friend bool operator>  (const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+				return lhs.c > rhs.c;
+			}
+
+			// >=
+			friend bool operator>= (const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
+				return lhs.c >= rhs.c;
+			}
+
+		protected:
+			container_type c;
+	};
+};
 
 #endif
---
+++ includes/test/test_stack/constructor/constructor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>       // std::cout
+#include <ft_stack.h>          // ft::stack
+#include <ft_vector.h>         // ft::vector
+#include <deque>          // std::deque
+
+int main ()
+{
+  std::deque<int> mydeque (3,100);         // deque with 3 elements
+  ft::vector<int> myvector (2,200);        // vector with 2 elements
+
+  ft::stack<int> first;                    // empty stack
+  ft::stack<int> second (mydeque);         // stack initialized to copy of deque
+
+  ft::stack<int,ft::vector<int> > third;   // empty stack using vector
+  ft::stack<int,ft::vector<int> > fourth (myvector);
+
+  std::cout << "size of first: " << first.size() << '\n';
+  std::cout << "size of second: " << second.size() << '\n';
+  std::cout << "size of third: " << third.size() << '\n';
+  std::cout << "size of fourth: " << fourth.size() << '\n';
+
+  return 0;
+}
