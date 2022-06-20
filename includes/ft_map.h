f8b8aa74db3c6b2abb6d8559c9b6b370b59175de
dclark <dclark@student.42.fr>
2022-06-20 12:08:48 +0200

    member types de map en cours

--- includes/ft_map.h
+++ includes/ft_map.h
@@ -6,7 +6,7 @@
 /*   By: david <dclark@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/19 11:51:52 by david             #+#    #+#             */
-/*   Updated: 2022/06/19 17:18:17 by david            ###   ########.fr       */
+/*   Updated: 2022/06/20 12:00:34 by dclark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,23 @@ namespace ft {
 		class Alloc = allocator<pair<const Key,T>> >
 	class map {
 		public:
-			/*-------- Member types --------*/
-			typedef typename 	Key									key_type;
-			typedef typename 	T									mapped_type;
-			typedef 			pair<const key_type, mapped_type>	value_type;
-			typedef typename	Compare								key_compare;
-			typedef				/*Something*/						value_compare;
-			typedef typename	Alloc								allocator_type;
-			typedef				allocator_type::reference			reference;
-			typedef				allocator_type::const_reference		const_reference;
-			typedef				allocator_type::pointer				pointer;
-			typedef				allocator_type::const_pointer		const_pointer;
-			typedef
+			/*-------- [MEMBER_TYPES] --------*/
+			typedef typename 	Key											key_type;
+			typedef typename 	T											mapped_type;
+			typedef 			pair<const key_type, mapped_type>			value_type;
+			typedef typename	Compare										key_compare;
+			typedef	typename	/*Something*/								value_compare;
+			typedef typename	Alloc										allocator_type;
+			typedef				allocator_type::reference					reference;
+			typedef				allocator_type::const_reference				const_reference;
+			typedef				allocator_type::pointer						pointer;
+			typedef				allocator_type::const_pointer				const_pointer;
+			typedef				/*Something*/								iterator;
+			typedef				/*Something*/								const_iterator;
+			typedef				ft::reverse_iterator<iterator>				reverse_iterator;
+			typedef				ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+			typedef				ft::iterator_traits<iterator>::difference_type	difference_type;
+			typedef				size_t										size_type;
 
 		private:
 	};
